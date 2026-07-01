c0a6d054ac8152ab109d4b6c3d973f6cb83965a6
Shemuel Castro <156734018+Shemuel-bot@users.noreply.github.com>
2026-07-01 18:05:55 +0000

    feat: add video player and enhance layout
    - Added react-player dependency for video playback functionality.
    - Updated App.css to improve header styling and add video section styles.
    - Modified App.jsx to include a ReactPlayer component for displaying a video.
    - Enhanced header with navigation links for better user experience.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,27 @@
-import { useState } from 'react'
-import {Link, Route, Routes} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import ReactPlayer from 'react-player'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <header>
+        <Link to="/">Courses</Link>
         <h1>Panda Bear Academy</h1>
         <Link to="/">About</Link>
       </header>
+
+      <main className="page">
+        <section className="video-section">
+          <ReactPlayer
+            url="https://www.youtube.com/watch?v=ZjXuTefizF0"
+            playing={false}
+            controls
+            width="100%"
+            height="100%"
+          />
+        </section>
+      </main>
     </>
   )
 }
