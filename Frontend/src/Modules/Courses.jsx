ca72b74316d2789079e426691fc357892fb12137
Shemuel Castro <156734018+Shemuel-bot@users.noreply.github.com>
2026-07-12 05:12:24 +0000

    feat: enhance Courses component with detailed course listings and categories

--- Frontend/src/Modules/Courses.jsx
+++ Frontend/src/Modules/Courses.jsx
@@ -3,19 +3,89 @@ export default function Courses() {
        <>
         <div>
             <h1>Your Courses</h1>
-            <ul>
-                <li>Course 1</li>
-                <li>Course 2</li>
-                <li>Course 3</li>
-            </ul>
+            <div>
+                <span>
+                    <img src="" alt="" />
+                    <div><h3>Calculus</h3></div>
+                </span>
+                <span>
+                    <img src="" alt="" />
+                    <div>
+                        <h3>Linear Algebra</h3>
+                    </div>
+                </span>
+            </div>
         </div>
         <div>
             <h1>Other Courses</h1>
-            <ul>
-                <li>Course 4</li>
-                <li>Course 5</li>
-                <li>Course 6</li>
-            </ul>
+            <div>
+                <h2>Math</h2>
+                <div>
+                    <span>
+                        <img src="" alt="" />
+                        <div><h3>Abstract Algebra</h3></div>
+                    </span>
+                    <span>
+                        <img src="" alt="" />
+                        <div><h3>Topology</h3></div>
+                    </span>
+                    <span>
+                        <img src="" alt="" />
+                        <div><h3>6th Grade Math</h3></div>
+                    </span>
+                </div>
+            </div>
+            <div>
+                <h2>Science</h2>
+                <div>
+                    <span>
+                        <img src="" alt="" />
+                        <div><h3>Physics</h3></div>
+                    </span>
+                    <span>
+                        <img src="" alt="" />
+                        <div><h3>Chemistry</h3></div>
+                    </span>
+                    <span>
+                        <img src="" alt="" />
+                        <div><h3>Biology</h3></div>
+                    </span>
+                </div>
+            </div>
+            <div>
+                <h2>History</h2>
+                <div>
+                    <span>
+                        <img src="" alt="" />
+                        <div><h3>World History</h3></div>
+                    </span>
+                    <span>
+                        <img src="" alt="" />
+                        <div><h3>US History</h3></div>
+                    </span>
+                    <span>
+                        <img src="" alt="" />
+                        <div><h3>European History</h3></div>
+                    </span>
+                </div>
+            </div>
+            <div>
+                <h2>Language</h2>
+                <div>
+                    <span>
+                        <img src="" alt="" />
+                        <div><h3>Spanish</h3></div>
+                    </span>
+                    <span>
+                        <img src="" alt="" />
+                        <div><h3>French</h3></div>
+                    </span>
+                    <span>
+                        <img src="" alt="" />
+                        <div><h3>German</h3></div>
+                    </span>
+                </div>
+            </div>
         </div>
        </>
     );
